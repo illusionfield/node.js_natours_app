ca980f9d408cbe0ac66401842cc714f6bfabd4b3
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-03-29 22:09:45 +0100

    Adding multiple middleware functions and serving static files

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,28 @@ const tours = JSON.parse(
 );
 
 // 2) ROUTE HANDLERS
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is:${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
@@ -21,16 +43,8 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   // it is a trick to convert string in the array to a number
   const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   // find the id in the array
   const tour = tours.find((el) => el.id === id);
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -59,13 +73,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -75,13 +82,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,

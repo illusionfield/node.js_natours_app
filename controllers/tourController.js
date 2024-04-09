1ffb9484e36464c37eb696a1b04a59f895fb6df8
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-04-09 23:26:07 +0200

    Implementing pagination feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,42 @@ exports.getAllTours = async (req, res) => {
     console.log(JSON.parse(queryStr));
 
     // find method going to return a query
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3) Sorting
+    if (req.query.sort) {
+      // we cannot leave a space in URL, so instead we're gonna add a comma
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      // if there is no query, the default is (createdAt)
+      query = query.sort('-createdAt');
+    }
+
+    // 4) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join('');
+      query = query.select(fields);
+    } else {
+      // if there is no query, the default is (- = not including, but excluding in the response, so except the "v" field)
+      query = query.select('-__v');
+    }
+
+    // 5) Pagination
+    // multiplying by one = from string will be a number
+    // defining a default value
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    // if page 3 the results are: (21 - 30), meaning that we need to skip 20 results, that basically 2 * 10(limit), that is (3-1) * 10(limit)
+    const skip = (page - 1) * limit;
+    // page=2&limit=10, 1-10, page 1, 11-20, page 2
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      // if the number of documents that we skip is greater than the number of documents, that actually exists
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
 
     // Execute the query
     const tours = await query;

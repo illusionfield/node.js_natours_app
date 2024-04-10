41eea95c55e0c1f9991b69f6670faeb156b934c8
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-04-10 20:01:04 +0200

    Implementing alias, to make the API better

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,13 @@ const Tour = require('./../models/tourModel');
 //  next();
 // };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // Build the query, so we first make a copy

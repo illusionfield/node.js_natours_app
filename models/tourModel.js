9ea903a6f58206fee28a631bceb0f947b7374db3
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-04-12 18:12:21 +0200

    Implementing aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -69,7 +69,8 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// Document middleware: runs before .save() and .create()
+// Document middleware
+// runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
@@ -86,6 +87,12 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds}`);
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: ture } } });
   next();
 });
 

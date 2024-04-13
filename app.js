8469ab4baa33d7488e9ae9a6cec302bbc4600129
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-04-13 16:07:38 +0200

    Implementing a global error handling middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorhandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -13,10 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -46,5 +45,14 @@ app.use((req, res, next) => {
 // Version 3.0 for refactoring the code for a better file structure
 app.use('/api/v1/tours', tourRouter); // middleware
 app.use('/api/v1/users', userRouter); // middleware
+
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorhandler);
 
 module.exports = app;

e0721dacd31bd17503a70e7103afcb243f6bb4af
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-04-26 20:22:48 +0200

    Adding handler factory functions: Delete

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,19 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.findByIdAndDelete(req.params.id);
+
+    if (!document) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+// handlerFactory = function, that returns another function, in this case our handler function, so for creating, reading, updating, deleting resources
+// genaralisation of this specific function (exports.deleteTour, exports.updateTour)

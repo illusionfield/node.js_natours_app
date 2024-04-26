4b9d1b59d1e7996f1b18d0ca37d2f4bbb19884b2
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-04-26 19:02:53 +0200

    Implementing nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// -------- Nested routes for reviews ----------- //
+
+// POST /tour/234fad4/reviews --> nested route
+// GET /tour/234fad4/reviews --> get us all the reviews for this tour
+// 234fad4 is just random
+
+// router
+//  .route('/:tourId/reviews')
+//  .post(
+//    authController.protect,
+//    authController.restrictTo('user'),
+//    reviewController.createReview
+//  );
+
+// for this specific route, we want to use reviewRouter
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
